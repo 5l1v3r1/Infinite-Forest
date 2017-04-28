db5811ee4a6aacb8fb36a8a986b0585d405f84c1
deadpix3l <skylerr.curtis@gmail.com>
2017-04-28 17:01:08 +0000

    improved gameloop and help messages

--- infiniteforest.cpp
+++ infiniteforest.cpp
@@ -4,40 +4,105 @@
 #include "Debug.h"
 using namespace std;
 
-int Tile::count = 0;
+//unsigned int Tile::count = 0;
 
 int main(int argc, char** argv)
 {
-    //seed rand()
+    // ##############################
+    // #         INIT PHASE         #
+    // ##############################
+
+    //produce good randomness
     srand(time(NULL));
-    
+
     //create all tiles
-    Tile *player = new Tile;
+    Tile *player = new Tile(Tile::grass);
     player->createEdges();
-    
-    //the game loop here
+
+    // welcome message.
+    cout << "==================================================" << endl;
+    cout << "Welcome to Infinite Forest." << endl;
+    cout << "A Generated Forest game that goes on forever." << endl;
+    cout << "==================================================" << endl;
+
+    // Mini How-To-Play
+    string helptext = "\ncontrols:\n\
+    [Enter] - repeat previous command\n\
+    n/s/e/w - move in the respective cardinal direction.\n\
+    h - show this help message\n\
+    d - show debug information.\n\
+    q - quit game.\n\n";
+    cout << helptext;
+
+    // ##############################
+    // #     GAME LOOP - REPL       #
+    // ##############################
+
     string input;
+    char action;
     while(1)
     {
         cout << "command> ";
         getline(cin, input);
-        
+        if(input.length()) action = input[0];
 
-        //support for full words (north, south, etc.) coming later
-        if(input=="s" || input=="n"|| input=="e"|| input=="w")
-        { player = player->movePlayer(input[0]); }
+        switch(tolower(action))
+        {
+            case 'h':
+                cout << helptext;
+                break;
 
-        //Debug
-        else if(input=="debug") { player->debugTile("Player"); }
-        else if(input=="debug all") { player->debugAllTiles("Player"); }
-        
-        else if(input=="quit") { break; }
-        else { cout << "sorry - we have not implemented that command - " << input[0] << endl; }
+            case 'n':
+            case 's':
+            case 'e':
+            case 'w':
+                player = player->movePlayer(action);
+                break;
+
+            case 'q':
+                //game cleanup - quitting
+                player->deleteEdges(15); // 1111 -> remove all nodes
+                delete player;
+                return 0;
+                break;
+
+            case 'd':
+                player->debugTile("Player");
+                break;
+
+            default:
+                cout << "Sorry, we havent inplemented that yet! ";
+                cout << "('" << action << "')" << endl;
+                break;
+        }
     }
-    
-    
-    //game cleanup
+
+    cout << "the loop ended improperly." << endl;
+    cout << "attempting cleanup...";
     player->deleteEdges(15); // 1111 -> remove all nodes
     delete player;
+    cout << "Goodbye!";
     return 0;
 }
+
+
+//init phase
+//1. create player tile.
+//2. create all tiles around player.
+
+//game loop - REPL
+//1. ask player for action
+//2. process action
+//3.. any output needed
+
+// Tile* init(void)
+// {
+//     //seed rand()
+//     srand(time(NULL));
+
+//     //create all tiles
+//     Tile *player = new Tile;
+//     player->createEdges();
+
+//     return player;
+// }
