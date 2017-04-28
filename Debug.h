48ca62f97f37c260f709ad5a8298f9250b70df80
deadpix3l <skylerr.curtis@gmail.com>
2017-04-28 16:42:03 +0000

    added support for tiletype names

--- Debug.h
+++ Debug.h
@@ -2,34 +2,37 @@
 
 //debugging constants and macros
 #define REVCHK(a,b) ((a->b==(this))? "true" : "false")
-const char* DEBUGFMT = "%c id: %d\t loc: %p\t rev: %s\n";
+//const char* DEBUGFMT = "%c id: %d\t loc: %p\t rev: %s\n";
+const char* DEBUGFMT = "%c --- type: %s\t loc: %p\t rev: %s\n";
+extern const char* Tilenames[];
 
 void Tile::debugTile(const char* name="Tile") 
 {
     printf("---------------------------\n");
     printf("%s - %p\n", name, (void *)this);
     printf("---------------------------\n");
-    printf("id: %d\t count: %d\t type: %d\n", id, count, tileType);
+    //printf("id: %d\t count: %d\t type: %d\n", id, count, tileType);
+    printf("type: %s\n", Tilenames[tileType]);
     
     if(n == NULL) {printf("N\t loc: NULL\n");}
-    else {printf(DEBUGFMT, 'N', n->id, (void *)n, REVCHK(n,s));}
+    else {printf(DEBUGFMT, 'N', Tilenames[n->tileType], (void *)n, REVCHK(n,s));}
     
     if(s == NULL) {printf("S\t loc: NULL\n");}
-    else {printf(DEBUGFMT, 'S', s->id, (void *)s, REVCHK(s,n));}
+    else {printf(DEBUGFMT, 'S', Tilenames[s->tileType], (void *)s, REVCHK(s,n));}
     
     if(e == NULL) {printf("E\t loc: NULL\n");}
-    else {printf(DEBUGFMT, 'E', e->id, (void *)e, REVCHK(e,w));}
+    else {printf(DEBUGFMT, 'E', Tilenames[e->tileType], (void *)e, REVCHK(e,w));}
     
     if(w == NULL) {printf("W\t loc: NULL\n");}
-    else {printf(DEBUGFMT, 'W', w->id, (void *)w, REVCHK(w,e));}
+    else {printf(DEBUGFMT, 'W', Tilenames[w->tileType], (void *)w, REVCHK(w,e));}
     
     printf("\n");
 }
 
 Tile::~Tile()
 {
-    printf("GoodBye Cruel World %p\n", (void *)this);
-    --count;
+    //printf("GoodBye Cruel World %p\n", (void *)this);
+    //--count;
 }
 
 void Tile::debugAllTiles(const char* name="Initial Tile")
